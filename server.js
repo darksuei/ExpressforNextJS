4f75ac0f99c06c6aa4b02e5cfa230d874ff50df5
Suei43 <sueiraphael@gmail.com>
2023-05-27 01:03:16 +0100

    Chore: update

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // Custom API endpoints or server-side routes
-  server.get('/api/example', (req, res) => {
-    // Handle the API request
-    res.json({ message: 'Hello from the API' });
-  });
+  // // Custom API endpoints or server-side routes
+  // server.get('/api/example', (req, res) => {
+  //   // Handle the API request
+  //   res.json({ message: 'Hello from the API' });
+  // });
 
   // Default Next.js handling
   server.all('*', (req, res) => {
